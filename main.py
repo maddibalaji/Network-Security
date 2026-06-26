beb55f548614deb8febcff7d6edab782cb7065f0
maddibalaj <maddi.24mim10049@vitbhopal.ac.in>
2026-06-26 14:37:27 +0530

    data validation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig  
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import  TrainingPipelineConfig    
 if __name__ == "__main__":
     try:
@@ -13,5 +14,13 @@ if __name__ == "__main__":
         logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        
+
+
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
